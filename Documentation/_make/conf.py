ce6a401def16597c06fb7586e752a3496c87ec45
Martin Bless <martin@mbless.de>
2015-11-02 12:15:39 +0100

    [TASK] Update conf.py for better PHP highlighting

--- Documentation/_make/conf.py
+++ Documentation/_make/conf.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# mb, 2015-10-01, 2015-10-21
+# mb, 2015-10-01, 2015-11-02
+
+# This file lives at https://github.com/marble/typo3-docs-typo3-org-resources/blob/master/userroot/scripts/bin/conf-2015-10.py
+# Check for a new version!
 
 # Generic conf.py for ALL projects.
 # Project specific settings should be in:
 # - Documentation/Settings.cfg (maintained by user)
-# - makedir/Defaults.sh        (maintained by admin)
-# - makedir/buildsettings.sh   (maintained by admin)
-# - makedir/Overrides.cfg      (maintained by admin)
+# - MAKEDIR/Defaults.sh        (maintained by admin)
+# - MAKEDIR/buildsettings.sh   (maintained by admin)
+# - MAKEDIR/Overrides.cfg      (maintained by admin)
 
 from cStringIO import StringIO
 import codecs
@@ -16,22 +19,41 @@ import os
 import sys
 import t3SphinxThemeRtd
 
+# enable highlighting for PHP code not between <?php ... ?> by default
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+lexers['php'] = PhpLexer(startinline=True)
+lexers['php-annotations'] = PhpLexer(startinline=True)
+
 
 # PART 1: preparations
 
 # the dictionary of variables of this module 'conf.py'
 G = globals()
 
+class WithSection:
+
+    def __init__(self, fp, sectionname):
+        self.fp = fp
+        self.sectionname = sectionname
+        self.prepend = True
+
+    def readline(self):
+        if self.prepend:
+            self.prepend = False
+            return '[' + self.sectionname + ']\n'
+        else:
+            return self.fp.readline()
+
 # get from 'buildsettings.sh'
-f1name = 'buildsettings.sh'
-contents = codecs.open(f1name, 'r', 'utf-8').read()
+
 # ConfigParser needs a section. Let's invent one.
 section = 'build'
-contents = '[' + section + ']\n'  + contents
-f1 = StringIO(contents)
 config = ConfigParser.RawConfigParser()
-config.readfp(f1)
-f1.close()
+f1name = 'buildsettings.sh'
+with codecs.open(f1name, 'r', 'utf-8') as f1:
+    config.readfp(WithSection(f1, section))
+
 # Required:
 MASTERDOC = config.get(section, 'MASTERDOC')
 BUILDDIR = config.get(section, 'BUILDDIR')
@@ -62,7 +84,7 @@ settingsabspath = projectabspath + '/' + 'Settings.cfg'
 
 # Better stop with exitcode=1 if there is no 'Settings.cfg' file.
 if not os.path.exists(settingsabspath):
-    sys.stderr('Settings.cfg not found')
+    sys.stderr.write('Settings.cfg not found\n')
     sys.exit(1)
 
 # user settings
@@ -78,7 +100,7 @@ if os.path.exists(settingsabspath):
         for o in config.options(s):
             US[s][o] = config.get(s,o)
 
-# If makedir/Defaults.cfg exists:
+# If MAKEDIR/Defaults.cfg exists:
 # Get defaults for settings that ARE NOT in Settings.cfg
 defaultsabspath = os.path.normpath(os.path.join(confpyabspath, '..', 'Defaults.cfg'))
 if os.path.exists(defaultsabspath):
@@ -89,18 +111,6 @@ if os.path.exists(defaultsabspath):
         for o in config.options(s):
             if not US[s].has_key(o):
                 US[s][o] = config.get(s,o)
-
-# If makedir/Overrides.cfg exists:
-# Set all settings thereby overriding existing ones.
-# So the admin has the last word
-overridesabspath = os.path.normpath(os.path.join(confpyabspath, '..', 'Overrides.cfg'))
-if os.path.exists(overridesabspath):
-    config = ConfigParser.RawConfigParser()
-    config.readfp(codecs.open(overridesabspath, 'r', 'utf-8'))
-    for s in config.sections():
-        US[s] = US.get(s, {})
-        for o in config.options(s):
-            US[s][o] = config.get(s,o)
 
 # some more preparations
 
@@ -179,78 +189,84 @@ extlinks['review'] = ('https://review.typo3.org/%s', 'review: ')
 intersphinx_mapping = {}
 
 
-
 # PART 3: apply user settings
 
+def updateModuleGlobals(GLOBALS, US):
+    if US.has_key('general'):
+        GLOBALS.update(US['general'])
+
+    # allow comma separated values like: .rst,.md
+    if type(GLOBALS['source_suffix']) in [type(''), type(u'')]:
+        GLOBALS['source_suffix'] = GLOBALS['source_suffix'].split(',')
+
+    # add extensions from user settings if legal
+    if US.has_key('extensions'):
+        for k,e in US['extensions'].items():
+            if e in legal_extensions and not e in GLOBALS['extensions']:
+                GLOBALS['extensions'].append(e)
+
+    if US.has_key('extlinks'):
+        for k, v in US['extlinks'].items():
+            # untested!
+            # we expect:
+            #     forge = https://forge.typo3.org/issues/%s | forge:
+            GLOBALS['extlinks']['k'] = (v.split('|')[0].strip(), v.split('|')[1].strip())
+
+    if US.has_key('intersphinx_mapping'):
+        for k, v in US['intersphinx_mapping'].items():
+            GLOBALS['intersphinx_mapping'][k] = (v, None)
+
+    if US.has_key('html_theme_options'):
+        for k, v in US['html_theme_options'].items():
+            GLOBALS['html_theme_options'][k] = v
+
+    LD = US.get('latex_documents', {})
+    GLOBALS['latex_documents'] = [(
+        LD.get('source_start_file') if LD.get('source_start_file') else  master_doc,
+        LD.get('target_name')       if 0                           else  'PROJECT' + '.tex',
+        LD.get('title')             if LD.get('title')             else  project,
+        LD.get('author')            if LD.get('author')            else  t3author,
+        LD.get('documentclass')     if 0                           else 'manual'
+    )]
+
+    LE = US.get('latex_elements', {})
+    GLOBALS['latex_elements'] = {
+        'papersize': LE.get('papersize') if 0 else 'a4paper',
+        'pointsize': LE.get('pointsize') if 0 else '10pt',
+        'preamble' : LE.get('preamble' ) if 0 else '\\usepackage{typo3}',
+        # for more see: http://sphinx-doc.org/config.html#confval-latex_elements
+    }
+
+    MP = US.get('man_pages', {})
+    GLOBALS['man_pages'] = [(
+        MP.get('source_start_file') if MP.get('source_start_file') else  master_doc,
+        MP.get('name')              if MP.get('name')              else  project,
+        MP.get('description')       if MP.get('description')       else  description,
+        [ MP.get('authors')         if MP.get('authors')           else  t3author ],
+        MP.get('manual_section')    if MP.get('manual_section')    else  1
+    )]
+
+    TD = US.get('texinfo_documents', {})
+    GLOBALS['texinfo_documents'] =[(
+        TD.get('source_start_file') if TD.get('source_start_file') else master_doc,
+        TD.get('target_name')       if TD.get('target_name')       else t3shortname,
+        TD.get('title')             if TD.get('title')             else project,
+        TD.get('author')            if TD.get('author')            else t3author,
+        TD.get('dir_menu_entry')    if TD.get('dir_menu_entry')    else project,
+        TD.get('description')       if TD.get('description')       else description,
+        TD.get('category')          if TD.get('category')          else 'Miscellaneous'
+    )]
+    return
 
 # NOW!
-# This is where the magic happens!
-# All remaining settings from 'general' are added here
-# as if they had been literally written here:
+# This is where the magic happens! We update this module's globals()
+# with the user settings US. That has the same effect as if the
+# settings had been part of this conf.py
 
-G.update(US['general'])
+updateModuleGlobals(G, US)
 
-# allow comma separated values like: .rst,.md
-if type(source_suffix) in [type(''), type(u'')]:
-    source_suffix = source_suffix.split(',')
 
-# add extensions from user settings if legal
-if US.has_key('extensions'):
-    for k,e in US['extensions'].items():
-        if e in legal_extensions and not e in extensions:
-            extensions.append(e)
-
-if US.has_key('extlinks'):
-    for k, v in US['extlinks'].items():
-        extlinks['k'] = v
-
-if US.has_key('intersphinx_mapping'):
-    for k, v in US['intersphinx_mapping'].items():
-        intersphinx_mapping[k] = (v, None)
-
-LD = US.get('latex_documents', {})
-latex_documents = [(
-    LD.get('source_start_file') if LD.get('source_start_file') else  master_doc,
-    LD.get('target_name')       if LD.get('target_name')       else  t3shortname + '.tex',
-    LD.get('title')             if LD.get('title')             else  project,
-    LD.get('author')            if LD.get('author')            else  t3author,
-    LD.get('documentclass')     if 0                           else 'manual'
-)]
-del LD
-
-LE = US.get('latex_elements', {})
-latex_elements = {
-    'papersize': LE.get('papersize') if 0 else 'a4paper',
-    'pointsize': LE.get('pointsize') if 0 else '10pt',
-    'preamble' : LE.get('preamble' ) if 0 else '\\usepackage{typo3}',
-    # 'figure_align'? better don't touch!
-    # 'figure_align': LE.get('figure_align') and 'htbp',
-}
-del LE
-
-MP = US.get('man_pages', {})
-man_pages = [(
-    MP.get('source_start_file') if MP.get('source_start_file') else  master_doc,
-    MP.get('name')              if MP.get('name')              else  project,
-    MP.get('description')       if MP.get('description')       else  description,
-    [ MP.get('authors')         if MP.get('authors')           else  t3author ],
-    MP.get('manual_section')    if MP.get('manual_section')    else  1
-)]
-del MP
-
-TD = US.get('texinfo_documents', {})
-texinfo_documents =[(
-    TD.get('source_start_file') if TD.get('source_start_file') else master_doc,
-    TD.get('target_name')       if TD.get('target_name')       else t3shortname,
-    TD.get('title')             if TD.get('title')             else project,
-    TD.get('author')            if TD.get('author')            else t3author,
-    TD.get('dir_menu_entry')    if TD.get('dir_menu_entry')    else project,
-    TD.get('description')       if TD.get('description')       else description,
-    TD.get('category')          if TD.get('category')          else 'Miscellaneous'
-)]
-del TD
-
-# set if not existing
+# define if not existing
 
 if not G.has_key('epub_author')      : epub_author    = t3author
 if not G.has_key('epub_copyright')   : epub_copyright = copyright
@@ -259,9 +275,9 @@ if not G.has_key('epub_title')       : epub_title     = project
 if not G.has_key('htmlhelp_basename'): htmlhelp_basename = t3shortname
 
 
-# PART 4: Overrides
+# PART 4: Overrides from 'conf.py'
 
-# Now we have the last word:
+# Now we set some overrides
 
 exclude_patterns = ['_make']
 html_last_updated_fmt = '%b %d, %Y %H:%M'
@@ -277,17 +293,18 @@ today_fmt = '%Y-%m-%d %H:%M'
 html_theme_options['docstypo3org'] = True
 
 
-# In the following: 'published' means:
-# Available under that name in the GlobalContext of Jinja2 templating.
-# Example for usage in the template:
-#   {{ show_copyright }}
+# 'published' means in the following context: Available under that
+# name in the GlobalContext of Jinja2 templating. Example: if
+# 'show_copyright' is in the published it can be used in the Jinja2
+# template like:
+#     {{ show_copyright }}
 
 
 # 'html_show_copyright' is published as 'show_copyright'
 html_show_copyright = True
 
 # published as 'theme_show_copyright'
-html_theme_options['show_copyright'] = html_show_copyright
+html_theme_options['show_copyright'] = True
 
 
 # published as 'theme_show_last_updated
@@ -302,40 +319,74 @@ html_theme_options['show_revision'] = True
 html_show_sourcelink = True
 
 # published as 'theme_show_sourcelink'
-html_theme_options['show_sourcelink'] = html_show_sourcelink
+html_theme_options['show_sourcelink'] = True
 
 
 # published as 'show_sphinx'
 html_show_sphinx = True
 
 # published as 'theme_show_sphinx'
-html_theme_options['show_sphinx'] = html_show_sphinx
+html_theme_options['show_sphinx'] = True
 
 
 # published as 'use_opensearch' (set above already)
 # html_use_opensearch = html_use_opensearch
 # '' is published as 'theme_use_opensearch
 # no trailing slash!
-html_use_opensearch = html_use_opensearch.rstrip('/')
-html_theme_options['use_opensearch'] = html_use_opensearch
+if not html_theme_options.has_key('use_opensearch'):
+    html_theme_options['use_opensearch'] = ''
+elif type(html_theme_options['use_opensearch']) in [type(''), type(u'')]:
+    html_theme_options['use_opensearch'] = html_theme_options['use_opensearch'].rstrip('/')
 
-# a bit of housekeeping: remove helper vars
+# ? html_use_opensearch = html_theme_options['use_opensearch']
 
-G = globals()
+# PART 5: Finally: Overrides.cfg has the last word
+#
+# .. attention::
+#
+#    This file is under control of the admin. Make sure its
+#    contents makes sense!
+#
+# If MAKEDIR/Overrides.cfg exists:
+# Set all settings thereby overriding existing ones.
+# So the admin has the last word
+
+# settings from Overrides.cfg
+OVERRIDES = {}
+
+overridesabspath = os.path.normpath(os.path.join(confpyabspath, '..', 'Overrides.cfg'))
+if os.path.exists(overridesabspath):
+    config = ConfigParser.RawConfigParser()
+    config.readfp(codecs.open(overridesabspath, 'r', 'utf-8'))
+    for s in config.sections():
+        OVERRIDES[s] = OVERRIDES.get(s, {})
+        for o in config.options(s):
+            OVERRIDES[s][o] = config.get(s,o)
+
+
+# AGAIN!
+# Let the magic happen. This time we apply 'overrides' to the module's globals()
+updateModuleGlobals(G, OVERRIDES)
+
+# As other modules of Sphinx check the values of conf.py let's do
+# a bit of housekeeping und remove helper vars that aren't needed any more.
+
 for k in ['f1', 'f1name', 'o', 'contents',
     'extensions_to_be_loaded', 'section', 'legal_extensions',
     'config', 'US', 'item', 's', 'v', 'e']:
     if G.has_key(k):
         del G[k]
-#del G, k
+del k
 
-f2name = logdirabspath + '/Settings.pprinted.txt'
+settingsLogFile = logdirabspath + '/Settings.pprinted.txt'
 
 if 0 and 'dump resulting settings':
     import pprint
     pprint.pprint(globals())
+
 if 1 and 'dump resulting settings to file':
     import pprint
-    f2 = codecs.open(f2name, 'w', 'utf-8')
+    f2 = codecs.open(settingsLogFile, 'w', 'utf-8')
     pprint.pprint(globals(), stream=f2)
     f2.close()
+    del f2
